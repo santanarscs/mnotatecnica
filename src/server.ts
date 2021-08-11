2f9b116f204f223d193bd070d5ec52688b96175c
Raphael Santana <raphaelstn@gmail.com>
2021-08-11 15:39:34 -0300

    update server ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import "reflect-metadata";
 import express, { Request, Response } from "express";
 import "./database";
+import { noteRoutes } from "./routes/notes.routes";
+import { interestedRoutes } from "./routes/interested.routes";
+import { itemsRoutes } from "./routes/items.routes";
+import { processesRoutes } from "./routes/processes.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/notes", noteRoutes);
+app.use("/interested", interestedRoutes);
+app.use("/items", itemsRoutes);
+app.use("/processes", processesRoutes);
 
 app.get("/", (request: Request, response: Response) =>
   response.json({ "Api Status": "Online 🖖", "Api version": "0.0.1" })
